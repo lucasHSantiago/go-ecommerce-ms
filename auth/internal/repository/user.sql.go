c4472f2efed3ff08b6d7fee575a7ce829d03ebc3
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-04-24 21:50:01 -0300

    feat: changed db folder to repository

--- auth/internal/repository/user.sql.go
+++ auth/internal/repository/user.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.28.0
 // source: user.sql
 
-package db
+package repository
 
 import (
 	"context"
--- auth/internal/repository/verifyemail.sql.go
+++ auth/internal/repository/verifyemail.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.28.0
 // source: verifyemail.sql
 
-package db
+package repository
 
 import (
 	"context"
