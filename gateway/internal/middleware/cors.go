204e125346281f5c435c379472eba12c5e3c7065
lucasHSantiago <lucashenriquesantiago@gmail.com>
2025-05-07 16:40:37 -0300

    feat: allow cors middleware

--- gateway/internal/middleware/cors.go
+++ gateway/internal/middleware/cors.go
@@ -1,4 +1,33 @@
 package middleware
 
-func AllowCors() {
+import (
+	"log/slog"
+	"net/http"
+	"strings"
+)
+
+func AllowCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	headers := []string{"*"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+
+	slog.Info("preflight request", "http_path", r.URL.Path)
 }
